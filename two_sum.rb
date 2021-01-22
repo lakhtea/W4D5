8ddbb597dcd8468e22d4d72330c80b0778ebd7aa
John Cigale <jcigale98@gmail.com>
2021-01-22 17:07:49 -0500

    finished two sum

--- two_sum.rb
+++ two_sum.rb
@@ -20,6 +20,20 @@
 #     false
 # end
 
-# arr = [0, 1, 5, 7]
+# arr = [0, 1, 5, 7] 
 #  p okay_two_sum?(arr, 6) # => should be true
 #  p okay_two_sum?(arr, 10) # => should be false
+
+def two_sum?(arr, target) # => 0(n) space 0(n)
+    hash = Hash.new(0)
+    arr.each_with_index {|ele, idx| hash[target - ele] = idx}
+    arr.each_with_index do |ele, idx|
+        if hash.has_key?(ele) && hash[ele] != idx
+            return true
+        end
+    end 
+    false
+end
+arr = [0, 1, 5, 7] 
+p two_sum?(arr, 6) # => should be true
+p two_sum?(arr, 10) # => should be false
