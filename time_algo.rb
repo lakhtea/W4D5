54866d1e86c5d42c7fb33a59e0861a31cd03065d
John Cigale <jcigale98@gmail.com>
2021-01-22 14:25:30 -0500

    finished first project

--- time_algo.rb
+++ time_algo.rb
@@ -17,7 +17,40 @@ def my_min(list) # => O(n)
 end
 
 
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)  # =>  -5  
+
+# def largest_contiguous_subsum(list) # => 0(n^2)
+#     sub_arrs = []
+#     (0...list.length).each do |i|
+#         (0...list.length).each do |j|
+#             sub_arrs << list[i..j]
+#         end
+#     end
+#     sub_arrs.max_by(&:sum).sum
+# end
+
+def largest_contiguous_subsum(list) # => 0(n)
+    max_sum = list.max
+    current_sum = list.first
+    (1...list.length).each do |i|
+        current_sum += list[i]
+        max_sum = current_sum if current_sum > max_sum
+        current_sum = 0 if current_sum < 0
+    end
+    max_sum
+end
+
+
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list)
+
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)  # =>  -5  
+p largest_contiguous_subsum(list)
 
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list)
 
+list = [5, 3, -7]
+p largest_contiguous_subsum(list)
